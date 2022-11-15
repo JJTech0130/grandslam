48902435a9ee4299bb46e41ae15b56d6c004a8e6
JJTech <jjtech@jjtech.dev>
2022-11-15 17:48:03 -0500

    remove if name == main

--- src/grandslam/gsa.py
+++ src/grandslam/gsa.py
@@ -453,20 +453,4 @@ def authenticate(username, password, anisette: Anisette):
         return
     else:
         print("Assuming 2FA is not required")
-
-
-if __name__ == "__main__":
-    # Try and get the username and password from environment variables
-    import os
-
-    username = os.environ.get("APPLE_ID")
-    password = os.environ.get("APPLE_ID_PASSWORD")
-    # If they're not set, prompt the user
-    if username is None:
-        username = input("Apple ID: ")
-    if password is None:
-        import getpass
-
-        password = getpass.getpass("Password: ")
-
-    authenticate(username, password, Anisette())
+        
